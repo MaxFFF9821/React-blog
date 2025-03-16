33ad999767f9ce7e0462bc2f5243c06bc6b2f895
maxfeng9821 <jsmax.f@hotmail.com>
2025-03-16 16:11:18 -0400

    Add Redux store setup, user slice, and login functionality; implement Axios request utility

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,5 +1,66 @@
+import './index.scss'
+import { Card, Form, Input, Button, message } from 'antd'
+import logo from '@/assets/logo.png'
+import { fetchLogin } from '@/store/modules/user'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 const Login = () => {
-  return <div>This is Login</div>
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const onFinish = async (value) => {
+    console.log(value);
+    await dispatch(fetchLogin(value))
+    navigate('/')
+    message.success('login success')
+    
+  }
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form
+          validateTrigger="onBlur"
+          onFinish={onFinish}
+        >
+          <Form.Item
+          name="mobile"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your phone number!',
+            },
+            {
+            
+              pattern: /^1[3-9]\d{9}$/,
+              message: 'Please enter the correct phonenumber'
+            }
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+          name="code"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your autentication!',
+            },
+
+          ]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
---
+++ src/store/modules/user.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { request } from "@/utils";
+
+const userStore = createSlice({
+  name: 'user',
+  initialState: {
+    token: ''
+  },
+  reducers: {
+    setToken(state, action) {
+      state.token = action.payload
+    }
+  }
+})
+
+const { setToken } = userStore.actions
+
+const userReducer = userStore.reducer
+
+//异步
+const fetchLogin = (loginForm) => {
+  return async (dispatch) => {
+    const res = await request.post('/authorizations', loginForm)
+    dispatch(setToken(res.data.token))
+  }
+}
+
+export { setToken,fetchLogin }
+
+export default userReducer
